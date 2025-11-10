10c61baabe273f3aab457f3b994f533822b28656
Jere Helenius <Jere.helenius2@gmail.com>
2025-11-10 11:41:49 +0200

    added skills

--- client/src/components/SkillsCard.jsx
+++ client/src/components/SkillsCard.jsx
@@ -3,7 +3,7 @@ export default function SkillsCard() {
     <div className="bg-gray-900 p-8 rounded-2xl shadow-xl border border-gray-800">
       <h2 className="text-2xl font-semibold mb-4">Core Skills</h2>
       <div className="flex flex-wrap justify-center gap-3 text-sm">
-        {["React", "Node.js", "Express", "MongoDB", "JavaScript", "TailwindCSS", "REST API", "Git / GitHub"].map((skill, i) => (
+        {["React", "Node.js", "Express", "MongoDB", "JavaScript", "TailwindCSS", "REST API", "Git / GitHub","HTML","CSS","Microsoft Copilot","ChatGPT"].map((skill, i) => (
           <span key={i} className="bg-gray-800 px-3 py-1 rounded-lg">{skill}</span>
         ))}
       </div>
